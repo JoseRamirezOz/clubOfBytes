158a02c1855867efb8182b800ddab564412ba40b
JoseRamirezOz <joe.broken@protonmail.com>
2022-09-11 17:02:00 -0500

    OK VERSION ESTABLE RESPONSIVE CELULARES

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+/*--------------------------------------------------------------------------------------------------
+    ANIMACION INDEX
+--------------------------------------------------------------------------------------------------*/
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 1000, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
